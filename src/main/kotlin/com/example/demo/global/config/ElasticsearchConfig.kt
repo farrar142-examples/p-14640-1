2d1ca657c5a06ac098704585f4bd13190c7d5fae
farrar142 <gksdjf1690@gmail.com>
2025-12-13 16:48:46 +0900

    c009

---
+++ src/main/kotlin/com/example/demo/global/config/ElasticsearchConfig.kt
@@ -0,0 +1,10 @@
+package com.example.demo.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories
+
+
+@Configuration
+@EnableElasticsearchRepositories
+class ElasticsearchConfig {
+}
